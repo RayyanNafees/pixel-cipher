b6ac8e59c06b55224cb68c67832bf22da914be36
RayyanNafees <nafees.rayyan@gmail.com>
2022-06-24 21:06:58 +0530

    Developed prototype for base converrsion

--- basify.py
+++ basify.py
@@ -1,29 +1,37 @@
-from math import log
+from math import log, ceil
 
 #* NOTE THAT math.log CAN GET THE POWER OF INTEGER OF ANY BASE
 
+strify = lambda arr: ''.join(str(i) for i in arr)
 
-def binary(n=68, base=2) -> dict:
-    
+
+def tobase(n, base=3):
+    assert base<=10, 'Not yet developed for higher bases'
+
     pow_dict = {}
 
     while n:
+        
         power = int(log(n, base)) # 6
-        pow_dict[power] = 1
-        n -=  base**power # n = 4
+        
+        mul = n//base**power
+        
+        pow_dict[power] = mul
+
+        n -=  (base**power)*mul # n = 4
 
     return pow_dict
 
-def digitify(pow_dict: dict, base: int=2) -> str:
+
+def digitify(pow_dict: dict, base: int=2, formatter=strify) -> str:
     strlen = max(pow_dict.keys())
-    strarr = ['0']*strlen
+    dig_arr = [0]*(strlen+1)
 
     for power, digit in pow_dict.items():
         if digit:
-            strarr[-(strlen- power)] = '1'  # same as strarr[power] = '1'; then doing reverse(strarr)
+            dig_arr[-(strlen- power)] = digit  # same as strarr[power] = '1'; then doing reverse(strarr)
             # Adds number from start using negatie indices
 
-    return ''.join(strarr)
+    return formatter(dig_arr)
 
-
-binify = lambda n, base=2: digitify(binary(n, base), base)
+binify = lambda n, base=2: digitify(tobase(n, base), base)
