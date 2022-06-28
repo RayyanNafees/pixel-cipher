161d2ffc26f5cca48222dcc192727f8ffd34c940
RayyanNafees <nafees.rayyan@gmail.com>
2022-06-28 12:11:15 +0530

    Finalised all compressors a=except pixelise

--- compressor.py
+++ compressor.py
@@ -1,3 +1,4 @@
+from click import FileError
 import basify
 from cipher import file_to_int as fileint, int_to_file as intfile
 
@@ -19,12 +20,6 @@ def compress(path, out=None):
         o.write(compressed_data)
 
 
-def compress_ascii(path, out): pass
-
-
-def pixelize(path, out): pass
-
-
 def uncompress(path: str, out=None):
     '''Uncompresses the compressed file
 
@@ -42,10 +37,50 @@ def uncompress(path: str, out=None):
     intfile(data, out)
 
 
-def uncompress_ascii(path, out): pass
+def compress_ascii(path, out) -> None:
+    '''Compresses a larger file to smaller output file with ext .ascii
+
+    Args:
+        path (str): File to compress
+        out (str): Output file path
+    '''
+    data = fileint(path)
+    compressed_data = basify.digitify(basify.base_ascii(data))
+
+    if not out:
+        out = path+'.ascii'
+    else:
+        return FileError(
+            "Extension can't be recognised, enter an output file name")
+
+    with open(out, 'w', encoding='utf-8') as o:
+        o.write(compressed_data)
+
+    return None
+
+
+def uncompress_ascii(path: str, out):
+    '''Uncompresses the compressed file
+
+    Args:
+        path (str): The file compressed data is stored in
+        out (str): The output file, remember the extension sha
+    '''
+    with open(path, encoding='utf-8') as f:
+        compressed_data = f.read()
+
+    if not out and path.endswith('.ascii'):
+        out = path[:-6]
+    else:
+        return FileError(
+            "Extension can't be recognised, enter an output file name")
+
+    data = basify.from_anybase(compressed_data, 65536, None, ord)
+    return intfile(data, out)
+
+#! Most imp functions left
+
+def pixelize(path, out): pass
 
 
 def unpixelize(path, out): pass
-
-compress('new.png', 'out2.txt')
-uncompress('out2.txt', 'new2.png')
