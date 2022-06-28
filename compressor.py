53d194312e50b4e71f52bd983e8aa06466a2089b
RayyanNafees <nafees.rayyan@gmail.com>
2022-06-28 15:34:51 +0530

    Added ascii compression while pixelising

--- compressor.py
+++ compressor.py
@@ -5,6 +5,7 @@ from filedata import (
     visualise,
     getrgb,
     unpixel)
+import os
 
 
 def compress(path, out=None):
@@ -53,14 +54,9 @@ def compress_ascii(path, out) -> None:
 
     if not out:
         out = path+'.ascii'
-    else:
-        return NameError(
-            "Extension can't be recognised, enter an output file name")
 
     with open(out, 'w', encoding='utf-8') as o:
         o.write(compressed_data)
-
-    return None
 
 
 def uncompress_ascii(path: str, out):
@@ -93,7 +89,6 @@ def pixelize(path: str, out: str = None) -> None:
     data = fileint(path)
     pixels = list(basify.base_pixel(data).values())
 
-
     if not out:
         out = path+'.png'
 
@@ -111,9 +106,37 @@ def unpixelize(path: str, out: str) -> None:
     pixels = getrgb(path)
     # data = sum(unpixel(pixel) for pixel in pixels)
 
-    data=basify.from_anybase(pixels, 16777216, None, unpixel)
+    data = basify.from_anybase(pixels, 16777216, None, unpixel)
 
     if not out and path.endswith('.png'):
-        out=path[:-4]
+        out = path[:-4]
 
     intfile(data, out)
+
+
+def pixelize_ascii(path: str, out: str = None) -> None:
+    '''Returns the compressed file image after ascii compression
+
+    Args:
+        path (str): Path to the file to compress
+        out (str): The output file path
+    '''
+    if not out:
+        out = path+'.ascii.png'
+    compress_ascii(path, 'temp.ascii')
+    pixelize('temp.ascii', out)
+    os.remove('temp.ascii')
+
+
+def unpixelize_ascii(path: str, out: str = None) -> None:
+    '''Converts the compressed file image back to the original file
+
+    Args:
+        path (str): Path to the ascii compressed file image
+        out (str, optional): Path to the destination file. Defaults to None.
+    '''
+    if not out and path.endswith('.ascii.png'):
+        out = path[:-10]
+    uncompress(path, 'temp.ascii')
+    uncompress_ascii('temp.ascii', out)
+    os.remove('temp.ascii')
