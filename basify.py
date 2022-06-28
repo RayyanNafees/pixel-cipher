6870e220ae4c0b284a6486309ac74c34743b5026
RayyanNafees <nafees.rayyan@gmail.com>
2022-06-28 09:18:51 +0530

    Added Better coding approach

--- basify.py
+++ basify.py
@@ -1,19 +1,23 @@
-from math import log, ceil
+from math import log
 import string
-from unicodedata import digit
 from cipher import pixel
-import numpy as np
+from functools import partial
 
-#* NOTE THAT math.log CAN GET THE POWER OF INTEGER OF ANY BASE
+# * NOTE THAT math.log CAN GET THE POWER OF INTEGER OF ANY BASE
 
-strify = lambda arr: ''.join(str(i) for i in arr)
-pixelise = lambda digits: [pixel(d) for d in digits]
+
+def strify(arr): return ''.join(str(i) for i in arr)
+def pixelise(digits): return [pixel(d) for d in digits]
+
 
 def isiterable(sequence):
     try:
-        for i in sequence: return True
-    except:
+        iter(sequence)
+    except TypeError:
         return False
+    else:
+        return True
+
 
 def tobase(n, base=3):
     '''Converts integers till any base till base-10
@@ -27,80 +31,103 @@ def tobase(n, base=3):
                         place there while writing the num
                         to be used in digitifying, the rest placevalues default to 0
     '''
-    
-    assert base<=10, 'Not yet developed for higher bases'
+
+    assert base <= 10, 'Not yet developed for higher bases'
 
     pow_dict = {}
 
     while n:
-        
-        power = int(log(n, base)) # 6
-        
+
+        power = int(log(n, base))  # 6
+
         mul = n//base**power
-        
+
         pow_dict[power] = mul
 
-        n -=  (base**power)*mul # n = 4
+        n -= (base**power)*mul  # n = 4
 
     return pow_dict
 
 
-def to_printable_base(n: int, base: int=11) -> dict[int, int]:
-  
+def to_printable_base(n: int, base: int = 11) -> dict[int, int]:
+    '''Converts an integer to base_printable
+
+    Args:
+        n (int): The integer to convert
+        base (int, optional): The base to convert it to (till base printable). Defaults to 11.
+
+    Returns:
+        dict[int, int]: A dictionary of powers to character representation for the int
+    '''
     base_chars = string.printable
-    assert base<= (len(base_chars)), 'Base out of range'
+    assert base <= (len(base_chars)), 'Base out of range'
 
     pow_dict = {}
 
     while n:
-        
-        power = int(log(n, base)) # 6
-        
+
+        power = int(log(n, base))  # 6
+
         mul = n//base**power    # value
         _mul = base_chars[mul]  # digit representation
-        
+
         pow_dict[power] = _mul
 
-        n -=  (base**power)*mul # n = 4
+        n -= (base**power)*mul  # n = 4
 
     return pow_dict
 
 
-to_base_ascii = lambda n, base: to_printable_base(n, base, ''.join(chr(i) for i in range(0, 65536)))
-
-to_base_unicode = lambda n, base: to_printable_base(n, base, ''.join(chr(i) for i in range(0, 1114111)))
+# to_base_ascii = lambda n, base: to_printable_base(n, base, )
 
 
-def to_anybase(n: int, base: int=37, base_chars=string.printable, char_func=None):
+def to_anybase(n: int, base: int = 37, base_chars: str = string.printable, char_func=None):
+    '''Convert an integer to any base with the provided char repr array or func that returns char repr
 
+    Args:
+        n (int): The integer to convert
+        base (int, optional): The base to raise the integer to. Defaults to 37.
+        base_chars (str, optional): The list of char repr indiced by their num value. Defaults to string.printable.
+        char_func (function, optional): A function that returns the char repr from the supplied num value. Defaults to None.
+
+    Returns:
+        _type_: _description_
+    '''
     functional = bool(char_func)
 
     if not functional:
-        assert base<= (len(base_chars)), 'Base out of range'
-
+        assert base <= (len(base_chars)), 'Base out of range'
 
     pow_dict = {}
 
     while n:
-        
-        power = int(log(n, base)) # 6
-        
+
+        power = int(log(n, base))  # 6
+
         mul = n//base**power    # value
 
         if not functional:
             _mul = base_chars[mul]  # digit representation from chars
         else:
-            _mul = char_func(mul)  # digit representation as out of func from a val
-        
+            # digit representation as out of func from a val
+            _mul = char_func(mul)
+
         pow_dict[power] = _mul
 
-        n -=  (base**power)*mul # n = 4
+        n -= (base**power)*mul  # n = 4
 
     return pow_dict
 
 
+base_ascii = partial(to_anybase, base_chars=''.join(chr(i)
+                     for i in range(0, 65536)))
 
-def from_any_base(n: int, base: int=37, base_chars=string.printable, num_func: function[[str], int]=None) -> int:
+base_unicode = partial(to_anybase, base_chars=''.join(chr(i)
+                       for i in range(0, 1114111)))
+
+
+#! So much vulnerability in this
+def from_anybase(n: int, base: int = 37, base_chars=string.printable, num_func=None) -> int:
     '''Reverse of to_any_base func
 
     Args:
@@ -112,52 +139,76 @@ def from_any_base(n: int, base: int=37, base_chars=string.printable, num_func: f
     Returns:
         int: Returns the integer it was converted from
     '''
+    assert base <= len(
+        base_chars), 'Not enough characters to reperesent higher int base'
+
     strn = str(n)
     num = 0
 
-    for n,i in enumerate(base_chars):
+    for _pow, ch in enumerate(reversed(strn)):
         mul = num_func or (lambda char: base_chars.index(char))
-        num += (base**n)*mul(i)
+        num += (base**_pow)*mul(ch)
+
     return num
 
 
+def digitify(pow_dict: dict[int, int], base: int = 2, formatter=strify, char_func=None) -> str:
+    '''Converts a dict of powers to char repr to a digit for tat power
 
-def digitify(pow_dict: dict[int, int], base: int=2, formatter=strify, char_func=None) -> str:
+    Args:
+        pow_dict (dict[int, int]): A dict mapping powers to char reprs
+        base (int, optional): The base of the integer. Defaults to 2.
+        formatter (function, optional): The format func the array of digits is passed from. Defaults to strify.
+        char_func (function, optional): The func to return the char repr in case pow_dict not given. Defaults to None.
 
+    Returns:
+        str: Returns the digit so formed ussually a string
+    '''
     strlen = max(pow_dict.keys())
     dig_arr = [0]*(strlen+1)
 
     for power, digit in pow_dict.items():
-        
-        if base<=10: # numeric digit
+
+        if base <= 10:  # numeric digit
             if digit:
 
-                dig_arr[(strlen- power)] = digit  
+                dig_arr[(strlen - power)] = digit
                 # same as strarr[power] = '1'
                 # then doing reverse(strarr)
                 # Adds number from start using negatie indices
 
-        elif base <= len(string.printable): # string digit
+        elif base <= len(string.printable):  # string digit
             if string.printable.index(digit):
-                dig_arr[(strlen- power)] = digit
+                dig_arr[(strlen - power)] = digit
 
         elif char_func:
             dig_arr[(strlen-power)] = char_func(digit)
-
 
     return formatter(dig_arr)
 
 #! NEEDED to DEBUG ----------------
 
-to_base_pixel = lambda n: digitify(to_anybase(n, 16777216, None, pixel), 16777216)
 
-to_base_pixel2=lambda n: digitify(to_anybase(n, 16777216, range(16777216) ), 16777216, lambda n: n, pixelise)
+to_base_pixel = partial(to_anybase, base=16777216,
+                        base_chars=None, char_func=pixel)
+to_base_pixels = partial(to_anybase, base=16777216, base_chars=range(16777216))
 
-#! Most crucial func ------------
 
-binify = lambda n, base=2: digitify(tobase(n, base), base)
-multify = lambda n, base=11: digitify(to_printable_base(n, base), base)
+def pixelify(n): return digitify(to_base_pixel(n), 16777216)
 
-test = lambda n=1000, base=11: print(n == int(str(multify(n, base)),base))
+
+def pixelify2(n): return digitify(
+    to_base_pixels(n), 16777216, lambda n: n, pixelise)
+
+
+#! Most crucial funcs above ------------
+
+
+def binify(n, base=2): return digitify(tobase(n, base), base)
+def multify(n, base=11): return digitify(to_printable_base(n, base), base)
+
+
+def test(n=1000, base=11): return print(n == int(str(multify(n, base)), base))
+
 
 test()
---
+++ logic.md
@@ -0,0 +1,21 @@
+# Logic
+
+## Ciphering
+
+1. Getting the numbers inside of a file
+
+2. Raise the integer to a higher base
+
+3. represent each character as a pixel value
+
+4. Form an image
+
+## Unciphering
+
+1. Get the pixel values from an image
+
+2. Convert them back to integers of base pixel
+
+3. Convert integer base_pixel to base_10
+
+4. Convert the base-10 integer back to bytes into the file
